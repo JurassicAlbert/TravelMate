bc55a525e9c6672cbe9b1301c88902eaf4fc9d6f
JurassicAlbert <albertjurasik@gmail.com>
2025-01-18 19:15:24 +0100

    add url for personalized travels

--- backend/travelmate/urls.py
+++ backend/travelmate/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .views.app_user_views import UserRegistrationView, UserLoginView, PasswordResetView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views.travel_location_views import TravelLocationViewSet, PersonalizedTravelRecommendationView
@@ -14,6 +16,18 @@ router.register(r'travel-locations', TravelLocationViewSet)
 router.register(r'travel-history', TravelHistoryViewSet)
 router.register(r'user-preferences', UserPreferencesViewSet)
 
+class RootAPIView(APIView):
+    """
+    Custom root API view to include all endpoints.
+    """
+    def get(self, request, *args, **kwargs):
+        return Response({
+            "travel-locations": request.build_absolute_uri('/travel-locations/'),
+            "travel-history": request.build_absolute_uri('/travel-history/'),
+            "user-preferences": request.build_absolute_uri('/user-preferences/'),
+            "travel-recommendations": request.build_absolute_uri('/travel-recommendations/'),
+        })
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', UserRegistrationView.as_view(), name='register'),
@@ -22,6 +36,7 @@ urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('password-reset/', PasswordResetView.as_view(), name='password-reset'),
     path('travel-recommendations/', PersonalizedTravelRecommendationView.as_view(), name='travel_recommendations'),
+    path('', RootAPIView.as_view(), name='root_api'),
     path('', include(router.urls)),
 ]
 
